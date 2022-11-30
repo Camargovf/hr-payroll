4238061e19cac4508927714f2e5c6f8be0a98ded
camargo <contato@valdeircamargo.com>
2022-11-30 11:40:22 -0300

    Feat - Payment class/service/resource

---
+++ src/main/java/com/camargovf/hrpayroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.camargovf.hrpayroll.services;
+
+import com.camargovf.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days) {
+        return new Payment("Bob", 200.0, days);
+    }
+}

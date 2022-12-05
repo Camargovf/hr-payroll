91dc0a9ee609f3dbdbceaee6401ba7d35d4b7749
camargo <contato@valdeircamargo.com>
2022-12-05 10:02:40 -0300

    Feat: Remove RestTemplate & add OpenFeignClient

--- src/main/java/com/camargovf/hrpayroll/services/PaymentService.java
+++ src/main/java/com/camargovf/hrpayroll/services/PaymentService.java
@@ -2,10 +2,9 @@ package com.camargovf.hrpayroll.services;
 
 import com.camargovf.hrpayroll.entities.Payment;
 import com.camargovf.hrpayroll.entities.Worker;
+import com.camargovf.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,17 +13,12 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", "" + workerId);
-        Worker worker = restTemplate.getForObject(workerHost + workerId, Worker.class); // "/workers/{id}", Worker.class, uriVariables);
+       Worker worker = workerFeignClient.findById(workerId).getBody(); //ResponseEntity getBody
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
